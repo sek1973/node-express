6e1d3556812a1ce7cb666d7810a3b585974129a3
Slawomir Kussowski <slawomirkussowski@gmail.com>
2021-10-15 20:27:36 +0200

    modified app to work with server

--- projects/client/src/app/app.component.ts
+++ projects/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, OnDestroy {
+  users: any = null;
+  private subscription: Subscription = Subscription.EMPTY;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.subscription.unsubscribe();
+    this.subscription = this.httpClient.get<any>('/api/users')
+      .subscribe(users => {
+        console.log(users);
+        this.users = users;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
